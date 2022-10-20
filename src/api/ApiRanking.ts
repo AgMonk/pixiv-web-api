4c76c396269d7c8e637115df2cbb97d98addc6fd
ginstone <hjg719@139.com>
2022-10-20 10:55:44 +0800

    绘画榜

--- src/api/ApiRanking.ts
+++ src/api/ApiRanking.ts
@@ -9,4 +9,16 @@ export class ApiRanking {
     }
 
     //todo 绘画榜
+    illust(param: {
+        p: number;
+        mode: `daily` | `weekly` | `monthly` | `rookie` | `original` | `male` | `female` | `daily_r18` | `weekly_r18` | `male_r18` | `female_r18`;
+        content: `all` | `illust` | `manga` | `ugoira`;
+        date?: string
+    }) {
+        const {date, content, p, mode} = param
+        const params = {date, content, p, mode, json: "format"}
+        return this.instance.get(`/ranking.php`, {params}).then(res => {
+            return res.data
+        })
+    }
 }
