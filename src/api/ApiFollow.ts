1d88d150fe25e3e65bf83d7d3dec871e655b41a0
ginstone <hjg719@139.com>
2022-10-20 09:33:00 +0800

    关注、取关

--- src/api/ApiFollow.ts
+++ src/api/ApiFollow.ts
@@ -10,5 +10,33 @@ export class ApiFollow {
     }
 
     //todo 关注
+    add(userId: number, restrict: 0 | 1, tag: string): Promise<any> {
+        let formData = new FormData();
+        formData.append("user_id", "" + userId)
+        formData.append("restrict", "" + restrict)
+        formData.append("tag", tag)
+        formData.append("mod", "add")
+        formData.append("type", "user")
+        formData.append("format", "json")
+        return this.instance.post("/bookmark_add.php", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                'x-csrf-token': this.token,
+            }
+        })
+    }
+
     //todo 取关
+    del(userId: number): Promise<any> {
+        let formData = new FormData();
+        formData.append("id", "" + userId)
+        formData.append("mod", "del")
+        formData.append("type", "bookuser")
+        return this.instance.post("/rpc_group_setting.php", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                'x-csrf-token': this.token,
+            }
+        })
+    }
 }
